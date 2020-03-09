ccf7d820902201da449b8970165bb846a391fff6
UrosJankovic <39414114+UrosJankovic@users.noreply.github.com>
2020-03-09 12:54:43 +0100

    Add files via upload

---
+++ Ver1/Ver1.ino
@@ -0,0 +1,153 @@
+#include <Wire.h>
+
+#define PWMA   6          //Left Motor Speed pin (ENA)
+#define AIN2   A0          //Motor-L forward (IN2).
+#define AIN1   A1          //Motor-L backward (IN1)
+#define PWMB   5           //Right Motor Speed pin (ENB)
+#define BIN1   A2          //Motor-R forward (IN3)
+#define BIN2   A3          //Motor-R backward (IN4)
+
+#define Addr  0x20
+
+int Speed = 60;
+byte value;
+
+
+void PCF8574Write(byte data);
+byte PCF8574Read();
+void forward();
+void backward();
+void right();
+void left();
+void stop();
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Wire.begin();
+  pinMode(PWMA,OUTPUT);                     
+  pinMode(AIN2,OUTPUT);      
+  pinMode(AIN1,OUTPUT);
+  pinMode(PWMB,OUTPUT);       
+  pinMode(AIN1,OUTPUT);     
+  pinMode(AIN2,OUTPUT);  
+  Serial.println("infrared Obstacle Avoidance example");
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  stop(); 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  PCF8574Write(0xC0 | PCF8574Read());   //set Pin High
+  value = PCF8574Read() | 0x3F;         //read Pin
+
+  bool Right = !(value & (1<<6));
+  bool Left = !(value & (1<<7));
+  
+//  if(value != 0xFF)
+//  {
+//    right();
+//    //left();
+//    delay(50);
+//    stop();
+//  }
+//  else
+//  {
+//     forward();
+//  }
+
+  if(!Right & !Left)
+  {
+    Serial.print("Forward");
+    forward();
+  }
+  else if (Right & Left) //right sensor
+  {
+    Serial.print("Backward");
+    backward();
+    delay(50);
+    stop();
+  }
+  else if (Left) //left sensor
+  {
+    Serial.print("Right");
+    right();
+    delay(50);
+    stop();
+  }
+  else
+  {      
+    Serial.print("Left");
+    left();
+    delay(50);
+    stop();
+  }
+}
+
+void PCF8574Write(byte data)
+{
+  Wire.beginTransmission(Addr);
+  Wire.write(data);
+  Wire.endTransmission(); 
+}
+
+byte PCF8574Read()
+{
+  int data = -1;
+  Wire.requestFrom(Addr, 1);
+  if(Wire.available()) {
+    data = Wire.read();
+  }
+  return data;
+}
+
+void forward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,LOW);  
+  digitalWrite(BIN2,HIGH); 
+}
+
+void backward()
+{
+  analogWrite(PWMA,Speed);
+  analogWrite(PWMB,Speed);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void right()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,HIGH);
+  digitalWrite(BIN1,HIGH); 
+  digitalWrite(BIN2,LOW);  
+}
+
+void left()
+{
+  analogWrite(PWMA,50);
+  analogWrite(PWMB,50);
+  digitalWrite(AIN1,HIGH);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,HIGH);  
+}
+
+void stop()
+{
+  analogWrite(PWMA,0);
+  analogWrite(PWMB,0);
+  digitalWrite(AIN1,LOW);
+  digitalWrite(AIN2,LOW);
+  digitalWrite(BIN1,LOW); 
+  digitalWrite(BIN2,LOW);  
+}
